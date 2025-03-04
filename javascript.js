eae25ef99d63f8cbf99f0abc9511abfde06f2385
Th3Catalyst <max675295@gmail.com>
2025-03-04 09:52:25 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    event.preventDefault();
-    const minusButton = document.getElementById("minusL1");
-    const plusButton = document.getElementById("plusL1");
-    const counterDiv = document.getElementById("cvL1");
-    const minusButton2 = document.getElementById("minusL2");
-    const plusButton2 = document.getElementById("plusL2");
-    const counterDiv2 = document.getElementById("cvL2");
-    const minusButton3 = document.getElementById("minusL3");
-    const plusButton3 = document.getElementById("plusL3");
-    const counterDiv3 = document.getElementById("cvL3");
-    let count1 = 0;
-    let count2 = 0;
-    let count3 = 0;
-});
+
+
+const minusButton = document.getElementById("minusL1");
+const plusButton = document.getElementById("plusL1");
+const counterDiv = document.getElementById("cvL1");
+const minusButton2 = document.getElementById("minusL2");
+const plusButton2 = document.getElementById("plusL2");
+const counterDiv2 = document.getElementById("cvL2");
+const minusButton3 = document.getElementById("minusL3");
+const plusButton3 = document.getElementById("plusL3");
+const counterDiv3 = document.getElementById("cvL3");
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+
 minusButton.addEventListener("click", (event) => {
     event.preventDefault();
     if (count1 > 0){
