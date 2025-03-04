d06622d0c23dbb41e4a404d64b5568250cfb31d8
Th3Catalyst <max675295@gmail.com>
2025-03-04 10:15:08 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,58 +1,61 @@
-
-const minusButton = document.getElementById("minusL1");
-const plusButton = document.getElementById("plusL1");
-const counterDiv = document.getElementById("cvL1");
-const minusButton2 = document.getElementById("minusL2");
-const plusButton2 = document.getElementById("plusL2");
-const counterDiv2 = document.getElementById("cvL2");
-const minusButton3 = document.getElementById("minusL3");
-const plusButton3 = document.getElementById("plusL3");
-const counterDiv3 = document.getElementById("cvL3");
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
-
-minusButton.addEventListener("click", (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
     event.preventDefault();
-    if (count1 > 0){
-        count1--;
-    }
-    counterDiv.innerHTML = count1;
-    console.log(count1);
-});
 
-plusButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    count1++;
-    counterDiv.innerHTML = count1;
-    console.log(count1);
+    const minusButton = document.getElementById("minusL1");
+    const plusButton = document.getElementById("plusL1");
+    const counterDiv = document.getElementById("cvL1");
+    const minusButton2 = document.getElementById("minusL2");
+    const plusButton2 = document.getElementById("plusL2");
+    const counterDiv2 = document.getElementById("cvL2");
+    const minusButton3 = document.getElementById("minusL3");
+    const plusButton3 = document.getElementById("plusL3");
+    const counterDiv3 = document.getElementById("cvL3");
+    let count1 = 0;
+    let count2 = 0;
+    let count3 = 0;
+    
+    minusButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count1 > 0){
+            count1--;
+        }
+        counterDiv.innerHTML = count1;
+        console.log(count1);
+    });
+    
+    plusButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        count1++;
+        counterDiv.innerHTML = count1;
+        console.log(count1);
+    });
+    
+    plusButton2.addEventListener("click", (event) => {
+        event.preventDefault();
+        count2++;
+        counterDiv2.innerHTML = count2;
+    });
+    
+    minusButton2.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count2 > 0){
+            count2--;
+        }
+        counterDiv2.innerHTML = count2;
+    });
+    
+    plusButton3.addEventListener("click", (event) => {
+        event.preventDefault();
+        count3++;
+        counterDiv3.innerHTML = count3
+    });
+    
+    minusButton3.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count3 > 0){
+            count3--;
+        }
+        counterDiv3.innerHTML = count3;
+    });
+    // nerd shi frfr
 });
-
-plusButton2.addEventListener("click", (event) => {
-    event.preventDefault();
-    count2++;
-    counterDiv2.innerHTML = count2;
-});
-
-minusButton2.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (count2 > 0){
-        count2--;
-    }
-    counterDiv2.innerHTML = count2;
-});
-
-plusButton3.addEventListener("click", (event) => {
-    event.preventDefault();
-    count3++;
-    counterDiv3.innerHTML = count3
-});
-
-minusButton3.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (count3 > 0){
-        count3--;
-    }
-    counterDiv3.innerHTML = count3;
-});
-// nerd shi frfr
