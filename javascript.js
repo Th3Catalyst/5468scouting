cf06b01736bcd42a5340afa058d51f531bcba9d7
Th3Catalyst <max675295@gmail.com>
2025-03-05 19:06:13 -0800

    tables

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     event.preventDefault();
 
+    //DESKTOP INCREMENTS
     const minusButton = document.getElementById("minusL1");
     const plusButton = document.getElementById("plusL1");
     const counterDiv = document.getElementById("cvL1");
+
     const minusButton2 = document.getElementById("minusL2");
     const plusButton2 = document.getElementById("plusL2");
     const counterDiv2 = document.getElementById("cvL2");
+
     const minusButton3 = document.getElementById("minusL3");
     const plusButton3 = document.getElementById("plusL3");
     const counterDiv3 = document.getElementById("cvL3");
+
+    const minusButton4 = document.getElementById("minusL4");
+    const plusButton4 = document.getElementById("plusL4");
+    const counterDiv4 = document.getElementById("cvL4");
+
     let count1 = 0;
     let count2 = 0;
     let count3 = 0;
+    let count4 = 0;
     
     minusButton.addEventListener("click", (event) => {
         event.preventDefault();
@@ -56,6 +65,100 @@ document.addEventListener("DOMContentLoaded", (event) => {
             count3--;
         }
         counterDiv3.innerHTML = count3;
+    });
+
+    plusButton4.addEventListener("click", (event) => {
+        event.preventDefault();
+        count4++;
+        counterDiv4.innerHTML = count4
+    });
+    
+    minusButton4.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count4 > 0){
+            count4--;
+        }
+        counterDiv4.innerHTML = count4;
+    });
+
+    //MOBILE INCREMENTS
+    const minusButtonM = document.getElementById("minusL1M");
+    const plusButtonM = document.getElementById("plusL1M");
+    const counterDivM = document.getElementById("cvL1M");
+
+    const minusButton2M = document.getElementById("minusL2M");
+    const plusButton2M = document.getElementById("plusL2M");
+    const counterDiv2M = document.getElementById("cvL2M");
+
+    const minusButton3M = document.getElementById("minusL3M");
+    const plusButton3M = document.getElementById("plusL3M");
+    const counterDiv3M = document.getElementById("cvL3M");
+
+    const minusButton4M = document.getElementById("minusL4M");
+    const plusButton4M = document.getElementById("plusL4M");
+    const counterDiv4M = document.getElementById("cvL4M");
+
+    let count1M = 0;
+    let count2M = 0;
+    let count3M = 0;
+    let count4M = 0;
+    
+    minusButtonM.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count1M > 0){
+            count1M--;
+        }
+        counterDivM.innerHTML = count1M;
+        console.log(count1M);
+    });
+    
+    plusButtonM.addEventListener("click", (event) => {
+        event.preventDefault();
+        count1M++;
+        counterDivM.innerHTML = count1M;
+        console.log(count1M);
+    });
+    
+    plusButton2M.addEventListener("click", (event) => {
+        event.preventDefault();
+        count2M++;
+        counterDiv2M.innerHTML = count2M;
+    });
+    
+    minusButton2M.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count2M > 0){
+            count2M--;
+        }
+        counterDiv2M.innerHTML = count2M;
+    });
+    
+    plusButton3M.addEventListener("click", (event) => {
+        event.preventDefault();
+        count3M++;
+        counterDiv3M.innerHTML = count3M;
+    });
+    
+    minusButton3M.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count3M > 0){
+            count3M--;
+        }
+        counterDiv3M.innerHTML = count3M;
+    });
+
+    plusButton4M.addEventListener("click", (event) => {
+        event.preventDefault();
+        count4M++;
+        counterDiv4M.innerHTML = count4M;
+    });
+    
+    minusButton4M.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (count4M > 0){
+            count4M--;
+        }
+        counterDiv4M.innerHTML = count4M;
     });
 
 
